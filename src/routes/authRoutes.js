d85a0b6947deccc6198be1c72ea5246a59a22231
Tazul Islam <tazulBinYounus@gmail.com>
2023-02-12 13:30:54 +0600

    Registration Complete

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,5 +2,6 @@ const authController = require("../controllers/authController.js");
 const router = require("express").Router();
 
 router.post("/login", authController.login);
+router.post("/registration", authController.registration);
 
 module.exports = router;
